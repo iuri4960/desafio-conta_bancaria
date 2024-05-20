6d7bcc76382e65aa118dae02c2f4647d866b6a24
iuri <iuriferbastos@gmail.com>
2024-05-20 12:40:06 -0300

    otimizando_desafio_bancario

---
+++ listas.py
@@ -0,0 +1,7 @@
+numeros= [1,7,20,4,12,1]
+pares= [numero for numero in numeros if numero %2 ==0]
+impar= [numero for numero in numeros if numero %2 != 0]
+print(pares)
+print(impar)
+q=numeros.count(1)
+print(q)
